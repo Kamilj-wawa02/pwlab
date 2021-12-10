c559f86658153442567d30a2e6789a8bc24bf7e9
Kamil Jablkowski <kamil.jablkowski5@gmail.com>
2021-12-10 19:00:47 +0100

    kod selekcji (Kamil)

--- trunk/src/selekcja.c
+++ trunk/src/selekcja.c
@@ -1,5 +1,20 @@
 #include "selekcja.h"
 
 void selekcja(Matrix *mat, Matrix *b, int c) { // operacja wykonywana jest dla konkretnej kolumny
-       
+
+        int elem = c;
+        for(int w = c + 1 ; w < mat->r; w++) { // sprawdzamy jedynie wiersze od diagonali w dół
+                if (fabs(mat->data[w][c]) > fabs(mat->data[elem][c]))
+                        elem = w;
+        }
+        if (elem != c) {
+                double *tmp = mat->data[c];
+                mat->data[c] = mat->data[elem];
+                mat->data[elem] = tmp;
+
+                tmp = b->data[c];
+                b->data[c] = b->data[elem];
+                b->data[elem] = tmp;
+        }
+
 }
