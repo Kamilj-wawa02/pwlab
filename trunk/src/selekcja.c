22327b2f806b7907a87c524d3a5d43dc263e4900
Pawel Borkowski <pawel.borkowski0704@gmail.com>
2021-12-10 19:05:05 +0100

    Kod selekcja

--- trunk/src/selekcja.c
+++ trunk/src/selekcja.c
@@ -6,9 +6,9 @@ void selekcja(Matrix *mat, Matrix *b, int k) { // operacja wykonywana jest dla k
 
        for(int w = k+1; w < mat->r ; w++)
 	       if( fabs(mat->data[w][k]) > fabs(mat->data[mx][k]) ); /* wybor elementu glownego w kolumnie */
-       		        	mx=w;
+       	        	mx=w;
 
-       /* Jesli mx != k , to zamieniam wiersze k i mx miejscami */
+        /* Jesli mx != k , to zamieniam wiersze k i mx miejscami */
        if ( mx != k ) {
 	    double *tmp = mat->data[k];
 	       mat->data[k] = mat->data[mx];
