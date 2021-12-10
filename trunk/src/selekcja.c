1dac1256b8e811ffa2ff92279f92a418f646f991
Pawel Borkowski <pawel.borkowski0704@gmail.com>
2021-12-10 19:00:46 +0100

    kod do selekcji

--- trunk/src/selekcja.c
+++ trunk/src/selekcja.c
@@ -1,5 +1,20 @@
 #include "selekcja.h"
+#include <math.h> //fabs()
 
-void selekcja(Matrix *mat, Matrix *b, int c) { // operacja wykonywana jest dla konkretnej kolumny
-       
+void selekcja(Matrix *mat, Matrix *b, int k) { // operacja wykonywana jest dla konkretnej kolumny
+       int mx = k;
+
+       for(int w = k+1; w < mat->r ; w++)
+	       if( fabs(mat->data[w][k]) > fabs(mat->data[mx][k]) ); /* wybor elementu glownego w kolumnie */
+       		        	mx=w;
+
+       /* Jesli mx != k , to zamieniam wiersze k i mx miejscami */
+       if ( mx != k ) {
+	    double *tmp = mat->data[k];
+	       mat->data[k] = mat->data[mx];
+	       mat->data[mx] = tmp;
+	    double tr = b->data[k];
+	       b->data[k] = b->data[mx];
+	       b->data[mx] = tr;
+       }
 }
